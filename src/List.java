4d0a56657355d53383ecb251039883ec061e5429
Rainer Willinger <rainer.willinger@aon.at>
2011-11-27 22:55:15 +0100

    new list implementation not really working

--- src/List.java
+++ src/List.java
@@ -4,7 +4,21 @@
  *
  */
 public class List<T> {
-	protected ListNode head = null;
+	/**
+	 * Attribut aktuelles Element.
+	 */
+	private ListNode current;
+ 
+	/**
+	 * Attribut erstes Element der Liste
+	 */
+	private ListNode first;
+ 
+	/**
+	 * Attribut letztes Element der Liste.
+	 */
+	private ListNode last;
+	/*protected ListNode head = null;
 	protected ListNode tail = null;
 	
 	public void add(T element) {
@@ -13,8 +27,47 @@ public class List<T> {
 		else {
 			// TODO previous is not working
 			tail.previous = tail = tail.next = new ListNode(element);
-			
 		}
+	}*/
+	
+	public void add(T element)
+	{
+		ListNode newElement;
+			if (this.isEmpty())
+			{// Erstes und einziges Element in die Liste einfügen
+				newElement = new ListNode(element, null, null); //(pObject, null, null);
+				this.first = newElement;
+				this.last = newElement;
+				this.current = null;
+			}
+			else
+			{ // Die Liste ist nicht leer
+				newElement = new ListNode(element, null, this.first); //pObject, null, this.first);
+					this.first.previous = newElement;
+					this.first = newElement;
+					this.current = null;
+				
+				/*else
+				{	// Der Positionszeiger steht mitten in der Liste.
+					newElement = new ListNode(element, this.current, this.current.next);
+					if (this.current == this.last)
+					{ // Nachfolger existiert nicht - Schwanzzeiger korregieren
+						this.last = newElement;
+					}
+					else
+					{ // Nachfolger korregieren
+						this.current.next.previous = newElement; //.getNext().setPrevious(newElement);
+					}
+					// Vorgänger koregieren
+					this.current.next = newElement; //setNext(newElement);
+					// Aktuelles Element bleibt aktuelles Element
+				}*/
+			}
+	}
+	
+	public boolean isEmpty()
+	{
+		return this.first == null;
 	}
 	
 	public Iter<T> iter() {
@@ -26,14 +79,16 @@ public class List<T> {
 		private ListNode next = null;
 		private ListNode previous = null;
 		
-		ListNode(T data) {
+		ListNode(T data, ListNode prev, ListNode next) {
 			this.data = data;
+			this.next = next;
+			this.previous = prev;
 		}
 		
 	}
 	
 	protected class ListIter implements Iter<T> {
-		protected ListNode n = head;
+		protected ListNode n = first;
 
 		@Override
 		public T next() {
